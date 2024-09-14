e275f4646554197567843f45a558f13fad5f4868
Minh Tan To <tominhtan500@gmail.com>
2024-09-14 18:22:56 +0000

    commit

--- RPS.py
+++ RPS.py
@@ -3,7 +3,7 @@
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
-    guess = "R"
+    guess = "P"
     if len(opponent_history) > 2:
         guess = opponent_history[-2]
 
